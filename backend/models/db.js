1370418bbf2d8b9c6a59b6480077e94c17cac0c7
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2025-12-25 16:21:06 +0200

    add db.js model

---
+++ backend/models/db.js
@@ -0,0 +1,23 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+const db = require('./index.cjs');
+
+export const { User,
+    Cart,
+    Brand,
+    Address,
+    Item,
+    ItemCategory,
+    ItemImage,
+    Message,
+    Notification,
+    Order,
+    OrderItem,
+    Product,
+    ProductCategory,
+    ProductImage,
+    PromoCode,
+    Review,
+    Wishlist } = db;
+export default db;
