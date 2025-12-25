da85a9102b4eebc7ede2aa7c6c50a9a8ef99257e
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2025-12-25 16:55:22 +0200

    fix seeders

--- backend/seeders/20251220231932-product_image.cjs
+++ backend/seeders/20251220231932-product_image.cjs
@@ -6,16 +6,16 @@ module.exports = {
     await queryInterface.bulkInsert(
       'ProductImages',
       [
-        { image_url: 'https://example.com/product1-1.jpg', product_id: 11, created_at: new Date(), updated_at: new Date() },
-        { image_url: 'https://example.com/product1-2.jpg', product_id: 11, created_at: new Date(), updated_at: new Date() },
-        { image_url: 'https://example.com/product2-1.jpg', product_id: 12, created_at: new Date(), updated_at: new Date() },
-        { image_url: 'https://example.com/product2-2.jpg', product_id: 12, created_at: new Date(), updated_at: new Date() },
-        { image_url: 'https://example.com/product3-1.jpg', product_id: 13, created_at: new Date(), updated_at: new Date() },
-        { image_url: 'https://example.com/product3-2.jpg', product_id: 13, created_at: new Date(), updated_at: new Date() },
-        { image_url: 'https://example.com/product4-1.jpg', product_id: 14, created_at: new Date(), updated_at: new Date() },
-        { image_url: 'https://example.com/product4-2.jpg', product_id: 14, created_at: new Date(), updated_at: new Date() },
-        { image_url: 'https://example.com/product5-1.jpg', product_id: 15, created_at: new Date(), updated_at: new Date() },
-        { image_url: 'https://example.com/product5-2.jpg', product_id: 15, created_at: new Date(), updated_at: new Date() },
+        { image_url: 'https://example.com/product1-1.jpg', product_id: 1, created_at: new Date(), updated_at: new Date() },
+        { image_url: 'https://example.com/product1-2.jpg', product_id: 1, created_at: new Date(), updated_at: new Date() },
+        { image_url: 'https://example.com/product2-1.jpg', product_id: 2, created_at: new Date(), updated_at: new Date() },
+        { image_url: 'https://example.com/product2-2.jpg', product_id: 2, created_at: new Date(), updated_at: new Date() },
+        { image_url: 'https://example.com/product3-1.jpg', product_id: 3, created_at: new Date(), updated_at: new Date() },
+        { image_url: 'https://example.com/product3-2.jpg', product_id: 3, created_at: new Date(), updated_at: new Date() },
+        { image_url: 'https://example.com/product4-1.jpg', product_id: 4, created_at: new Date(), updated_at: new Date() },
+        { image_url: 'https://example.com/product4-2.jpg', product_id: 4, created_at: new Date(), updated_at: new Date() },
+        { image_url: 'https://example.com/product5-1.jpg', product_id: 5, created_at: new Date(), updated_at: new Date() },
+        { image_url: 'https://example.com/product5-2.jpg', product_id: 5, created_at: new Date(), updated_at: new Date() },
       ],
       {}
     );
