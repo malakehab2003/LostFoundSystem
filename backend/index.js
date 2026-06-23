15ed341831628d136a59e672d8c2a7007a96450a
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2026-06-23 12:22:14 +0300

    fix index

--- backend/index.js
+++ backend/index.js
@@ -17,11 +17,6 @@ initSocket(server);
 
 app.use(logger("dev"));
 app.use(cors());
-app.post(
-  "/api/payment/webhook",
-  express.raw({ type: "application/json" }),
-  stripeWebhook
-);
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
